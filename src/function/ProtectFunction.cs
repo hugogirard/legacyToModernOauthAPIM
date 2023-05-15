027f93b475ce37f538d4156901edaf461d949046
Hugo Girard <hugirard@microsoft.com>
2023-05-15 09:14:33 -0400

    remove function key

--- src/function/ProtectFunction.cs
+++ src/function/ProtectFunction.cs
@@ -14,7 +14,7 @@ namespace Contoso
     {
         [FunctionName("ProtectFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
